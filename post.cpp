35b71715ba4245a2ccba1d5440b5e04bb5528c48
jlaguna <elagunaj@gmail.com>
2015-03-03 20:01:45 -0800

    initial commit

---
+++ post.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int clean()
+{
+    char comments;
+}
+
+int main()
+{
+    char x[50];
+    while (clean() && in.good)
+    {
+        in>>x;
+    }
+    return 0;
+}
